b924c232c9c99cef97b59a3d99b5adb1f426cf4b
salamidrus <idrussalam95@gmail.com>
2021-05-25 07:28:55 +0700

    Add String Methods

---
+++ working_with_string/string.rb
@@ -0,0 +1,9 @@
+puts "Idrus \n Academy" # new line
+phrase = "Idrus Academy"
+puts phrase.upcase()
+puts "    lead and trailing space     ".strip()
+puts "Idrus".length()
+puts phrase.include? "aca"
+puts phrase[0]
+puts phrase[0,3]
+puts phrase.index("a")
