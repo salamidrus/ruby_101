539e9e62fdadfbd3712b36095c94116c5a3218e0
salamidrus <idrussalam95@gmail.com>
2021-05-25 14:34:51 +0700

    Add Hashes

---
+++ hashes/hashes.rb
@@ -0,0 +1,11 @@
+states = {
+    "Pennsylvania" => "PA",
+    "New York" => "NY",
+    "Oregon" => "OR",
+    1 => "ONE",
+    :Palembang => "PLM"
+}
+
+puts states["New York"]
+puts states[1]
+puts states[:Palembang]
